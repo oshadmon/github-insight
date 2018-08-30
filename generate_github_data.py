ecf4cb873b416efd2aa7fd848c1743480a9e807e
Ori Shadmon <oshadmon@gmail.com>
2018-08-30 13:37:53 -0700

    updated script to include referrals but not commits by user

--- generate_github_data.py
+++ generate_github_data.py
@@ -1,13 +1,12 @@
 #!/bin/user/python
 """
 Name: Ori Shadmon
-Date: June 2018 
-Description: The following code takes GitHub connection info, and generates traffic, commits, and clones insight. 
-             This data is stored in JSON files under /tmp 
+Date: August 2018 
+Description: The following code takes GitHub connection info, and generates traffic, commits, clones and referrals insigh. 
+             The result is then saved into a JSON file.  
 """
 import aiohttp
 import argparse
-import asyncio
 import datetime
 import json
 import os
@@ -16,24 +15,23 @@ import requests
 import threading
 import uuid
 
-loop = asyncio.get_event_loop()
 
 def get_timestamp(que:queue.Queue=None)->str:
    """
    Create a current timestamp
    """
-   timestamp=datetime.datetime.now() 
-   timestamp=timestamp.strftime('%Y_%m_%d')
-   que.put(timestamp)
+   timestamp=datetime.datetime.now()
+   que.put(timestamp.strftime('%Y-%m-%d %H:%M:%S'))
 
-def send_request(traffic_que:queue.Queue=None, commits_que:queue.Queue=None, clones_que:queue.Queue()=None,  repo:str='', organization:str='', 
-                 auth_pair:tuple=())->(requests.models.Response, requests.models.Response, requests.models.Response): 
+def send_request(traffic_que:queue.Queue=None, commits_que:queue.Queue=None, clones_que:queue.Queue()=None,  
+                 referrers_que:queue.Queue=None, repo:str='', organization:str='', auth_pair:tuple=())->(requests.models.Response, requests.models.Response, requests.models.Response, requests.models.Response): 
    """
    Send request to specific Github API endpoint
    :param:
       traffic_que:queue.Queue - que to store traffic results 
       commits_que:queue.Queue - que to store commits reults 
       clones_que:queue.Queue - que to store clones results 
+      referrers_que:queue.Queue - que to store referrers result
       repo:str - repository name
       organization:str - organization name 
       auth_pair:tuple - user/password tuple pair
@@ -59,21 +57,22 @@ def send_request(traffic_que:queue.Queue=None, commits_que:queue.Queue=None, clo
    base_url = base_url + organization + '/' + repo + '/traffic/clones'
    clones_response = requests.get(base_url, auth=auth_pair)
 
+   # referrers 
+   base_url = 'https://api.github.com/repos/'
+   referrers_url = base_url + organization + '/' + repo + '/traffic/popular/referrers'
+   referrers_response = requests.get(referrers_url, auth=auth_pair)
+
    traffic_que.put(traffic_response) 
    commits_que.put(commits_response)
    clones_que.put(clones_response)
+   referrers_que.put(referrers_response)
   
-
-def read_traffic(traffic:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list:
+def read_traffic(traffic:requests.models.Response=None, repo:str='repo_name')->list:
    """
-   Create list of JSON objects with traffic info, and send to FogLAMP  
+   Create list of JSON objects with traffic info
    :param: 
       traffic:requests.models.Response - Object with traffic info
       repo:str - repository name 
-      json_dir:str - directory that will store json object
-      timestamp:str - date when data is generated
-      host:str - FogLAMP POST host 
-      port:int - FogLAMP POST port 
    :sample: 
    {
         "timestamp" : "2018-06-08T00:00:00Z"
@@ -93,19 +92,17 @@ def read_traffic(traffic:requests.models.Response=None, repo:str='repo_name', ti
                  })
    return data
 
-def read_commits_timestamp(commits:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list:
+def read_commits(commits:requests.models.Response=None, repo:str='repo_name')->list:
    """
-   Create list of JSON objects with commits (by timestamp) info, and send to FogLAMP 
+   Create list of JSON objects with commits (by timestamp) info
    :param: 
       commits:requests.models.Response - Object with commit info
       repo:str - repository name
-      json_dir:str - directory that will store json object
-      timestamp:str - date when data is generated
    :sample: 
    {
         "timestamp" : "2018-06-18"
         "key"       : "ff7a5466-7c0a-11e8-ab26-0800275d93ce"
-        "asset"     : "github/repo_name/commits/timestamp"
+        "asset"     : "github/repo_name/commits"
         "readings"  : {"commits/timestamp" : 12}
    }
    """
@@ -115,71 +112,30 @@ def read_commits_timestamp(commits:requests.models.Response=None, repo:str='repo
    timestamps={} 
    for i in range(len(commits)): 
       timestamp=commits[i]['commit']['author']['date']
-      timestamp=datetime.datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+      timestamp=timestamp.split('T')[0]
       if timestamp not in timestamps: 
          timestamps[timestamp]=0 
       timestamps[timestamp]+=1 
 
    # Create file with JSON object based on timestamps dict 
    data=[]
-   for key in timestamps: 
-      data.append({'timestamp' : str(key),
+   for timestamp in timestamps: 
+      data.append({'timestamp' : str(timestamp),
                    'key'       : str(uuid.uuid1()),
-                   'asset'     : 'github/%s/commits/timestamp' % repo, 
-                   'readings'  : {'commits/timestamp' : timestamps[key]}
+                   'asset'     : 'github/%s/commits' % repo, 
+                   'readings'  : {'commits' : timestamps[timestamp]}
                  })
    return data 
 
-def read_commits_users(commits:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list:
-   """
-   Create list of JSON objects with commits (by user) info, and send to FogLAMPe
-   :param: 
-      commitsrequests.models.Response=None - Object with commit info
-      repo:str - repository name
-      json_dir:str - directory that will store json object
-      timestamp:str - date when data is generated
-   :sample: 
-   {
-        "timestamp" : "2018-06-21 15:30:09.537268"
-        "key"       : "ff7a5466-7c0a-11e8-ab26-0800275d93ce"
-        "asset"     : "github/repo_name/commits/users/Ivan_Zoratti"
-        "readings"  : {"commits/users/Ivan_Zoratti" : 2}
-   }
-   """
-   commits=commits.json()
-   users={} 
-   data=[]
-   for i in range(len(commits)): 
-      user=commits[i]['commit']['author']['name']
-      if user not in users: 
-         users[user]=0
-      users[user]+=1
-
-   data.append({'timestamp' : str(datetime.datetime.now()),    
-                'key'       : str(uuid.uuid1()),
-                'asset'     : 'github/%s/commits/users' % repo, 
-                'readings'  : {'commits/users' : int(len(users))}
-              })
-   # number of commits per user 
-   for key in users: 
-      data.append({'timestamp' : str(datetime.datetime.now()), 
-                   'key'       : str(uuid.uuid1()),
-                   'asset'     : 'github/%s/commits/users/%s' % (repo, key.replace(' ','-').replace('_', '-')),
-                   'readings'  : {'commits/users/%s' % key.replace(' ','-').replace('_', '-') : users[key]}
-                 }) 
-   return data
-
-def read_clones(clones:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now().strftime('%Y_%m_%d'))->list: 
+def read_clones(clones:requests.models.Response=None, repo:str='repo_name')->list: 
    """
    Create list of JSON objects with daily clones, and write to file 
    :param: 
       clones:requests.models.Response - Object with clones info 
       repo:str - repository name
-      json_dir:str - directory that will store json object
-      timestamp:str - date when data is generated
    :sample: 
    {
-        "timestamp" : "2018-06-08T00:00:00Z"
+        "timestamp" : "2018-06-08"
         "key"       : "ff7a5466-7c0a-11e8-ab26-0800275d93ce"
         "asset"     : "github/repo_name/clones"
         "readings" : {"clones" : 5}
@@ -188,7 +144,6 @@ def read_clones(clones:requests.models.Response=None, repo:str='repo_name', time
    clones=clones.json()
    data=[]
    for key in clones['clones']:
-      timestamp=datetime.datetime.strptime(key['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
       data.append({'timestamp' : str(key['timestamp']),
                    'key'       : str(uuid.uuid1()),
                    'asset'     : 'github/%s/clones' % repo,
@@ -196,6 +151,30 @@ def read_clones(clones:requests.models.Response=None, repo:str='repo_name', time
                  })
    return data 
 
+def read_referrers(referrers:requests.models.Response=None, repo:str='repo_name', timestamp:str=datetime.datetime.now())->list:
+   """
+   Create a list of JSON objects with daily referrers 
+   :param: 
+      referrers:requests.models.Response - Object with referral info
+      repo:str - reposiitory name
+      timestamp:str - date when data is generated
+   :sample:
+   {
+   }
+   """
+
+   referrers=referrers.json()
+   data=[] 
+   # {'count': 79, 'uniques': 19, 'referrer': 'Google'}
+   for obj in referrers: 
+      data.append({'timestamp': str(timestamp),
+             'key': str(uuid.uuid1()),
+             'asset': 'github/%s/referrerals/%s' % (repo, obj['referrer']), 
+             'readings': {'count': obj['uniques'], # unique 
+                          'total': obj['count'] 
+                         }
+           })
+   return data
 
 def write_to_file(file_name:str='/tmp/data.json', data:list=[]):
    """
@@ -239,33 +218,31 @@ def main():
     traffic_que=queue.Queue()
     commits_que=queue.Queue()
     clones_que=queue.Queue()
-    threads=[threading.Thread(target=get_timestamp, args=(timestamp_que,)), 
-             threading.Thread(target=send_request,  args=(traffic_que, commits_que, clones_que, repo, org, auth,))
-            ]    
-    for thread in threads:
-       thread.start()
-    for thread in threads:
-       thread.join()
-    timestamp=timestamp_que.get()
-    traffic_request=traffic_que.get() 
-    commits_request=commits_que.get() 
-    clones_request=clones_que.get() 
-
-    traffic_data=read_traffic(traffic_request, repo, timestamp) 
-    commits_timestamp_data=read_commits_timestamp(commits_request, repo, timestamp)
-    commits_users_data=read_commits_users(commits_request, repo, timestamp) 
-    clones_data=read_clones(clones_request, repo, timestamp) 
+    referrers_que=queue.Queue()
     
-    file_name=args.dir+'/%s_github_%s_data.json' % (datetime.datetime.now().strftime('%Y_%m_%d'), repo) 
-    payload=[traffic_data, commits_timestamp_data, commits_users_data, clones_data]
-    loop = asyncio.get_event_loop()
+    threads=[
+             threading.Thread(target=get_timestamp, args=(timestamp_que,)), 
+             threading.Thread(target=send_request,   args=(traffic_que, commits_que, clones_que, referrers_que, repo, org, auth,))
+            ]
+    for thread in threads: 
+       thread.start()
+    for thread in threads: 
+       thread.join()
+ 
+    timestamp=timestamp_que.get()
+    traffic=traffic_que.get()
+    commits=commits_que.get()
+    clones=clones_que.get()
+    referrers=referrers_que.get() 
 
-    open(file_name, 'w').close()
-    write_to_file(file_name, traffic_data)
-    write_to_file(file_name, commits_timestamp_data)
-    write_to_file(file_name, commits_users_data) 
-    write_to_file(file_name, clones_data)
-     
+    file_name=args.dir+'/%s_%s_data.json' % (timestamp.replace(' ','_').replace(':', '_'), repo)
+    write_to_file(file_name, read_traffic(traffic, repo))
+    write_to_file(file_name, read_commits(commits,repo))
+    write_to_file(file_name, read_clones(clones, repo)) 
+    write_to_file(file_name, read_referrers(referrers, repo, timestamp.split(' ')[0]))
+
+
+
 
 if __name__ == '__main__': 
    main()
