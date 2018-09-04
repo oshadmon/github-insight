75597dbb6cb97a6b28c0fcc53d54e326484f729b
Ori Shadmon <oshadmon@gmail.com>
2018-09-04 16:53:45 -0700

    updated create_table

--- create_table.sql
+++ create_table.sql
@@ -7,7 +7,7 @@ CREATE TABLE github_traffic(
    row_id    VARCHAR(255) NOT NULL DEFAULT 'c8e968cd-fb59-4d65-8f65-d88d9bf7b03c', 
    timestamp DATE         NOT NULL DEFAULT '2000-01-01',
    repo      VARCHAR      NOT NULL DEFAULT 'new repo',
-   traffic   INT          NOT NULL DEFAULT 0, 
+   today     INT          NOT NULL DEFAULT 0, 
    total     INT          NOT NULL DEFAULT 0, -- value is calculated as SUM(traffic)
    PRIMARY KEY (row_id) -- This is "legal" only because column is really of UUID type
 ); 
@@ -16,7 +16,7 @@ CREATE TABLE github_commits(
    row_id VARCHAR(255) NOT NULL DEFAULT 'c8e968cd-fb59-4d65-8f65-d88d9bf7b03c',   
    timestamp DATE         NOT NULL DEFAULT '2000-01-01',
    repo      VARCHAR      NOT NULL DEFAULT 'new repo',
-   commits   INT          NOT NULL DEFAULT 0,   
+   today     INT          NOT NULL DEFAULT 0,   
    total     INT          NOT NULL DEFAULT 0, -- value is calculated as SUM(commits)
    PRIMARY KEY (row_id) -- This is "legal" only because column is really of UUID type
 );
@@ -25,7 +25,7 @@ CREATE TABLE github_clones(
    row_id    VARCHAR(255) NOT NULL DEFAULT 'c8e968cd-fb59-4d65-8f65-d88d9bf7b03c',   
    timestamp DATE         NOT NULL DEFAULT '2000-01-01',
    repo      VARCHAR      NOT NULL DEFAULT 'new repo',
-   clones    INT          NOT NULL DEFAULT 0,   
+   today     INT          NOT NULL DEFAULT 0,   
    total     INT          NOT NULL DEFAULT 0, -- value is calculated as SUM(clones)
    PRIMARY KEY (row_id) -- This is "legal" only because column is really of UUID type
 ); 
@@ -39,6 +39,3 @@ CREATE TABLE github_referrals(
    total     INT          NOT NULL DEFAULT 0,
    PRIMARY KEY (row_id) -- This is "legal" only because column is really of UUID type
 );
-
-
-
