eb9b188abd996ee13213be8e02a0ce7126e9df18
Ori Shadmon <oshadmon@gmail.com>
2018-09-17 10:35:18 -0700

    updated script to execute both CREATE and insert based oon aws_ip_list

--- aws/aws_downloads.sql
+++ aws/aws_downloads.sql
@@ -1,6 +1,9 @@
 DROP TABLE IF EXISTS aws_downloads; 
 CREATE TABLE aws_downloads( 
-   create_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-   ip          VARCHAR(255) NOT NULL DEFAULT '127.0.0.1'
+   create_date DATE     NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+   repo        VARCHAR  NOT NULL DEFAULT 'AWS', 
+   ip_count    int  NOT NULL DEFAULT 0 
 ); 
+
+INSERT INTO aws_downloads(create_date, ip_count) SELECT create_date, COUNT(ip) FROM aws_ip_list GROUP BY create_date ORDER BY create_date; 
 
